2acd66855ab6a68db6bc401c5ee30e80f0f269ee
Ruben Oost <mail@rubenoost.nl>
2014-11-19 09:33:48 +0100

    Project aangemaakt

---
+++ Planets/PlanetsLauncher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Planets
+{
+    static class PlanetsLauncher
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PlanetsForm());
+        }
+    }
+}
