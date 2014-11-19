b6a878291b768e75f1f83072fc0b61c1448089d0
Ruben Oost <mail@rubenoost.nl>
2014-11-19 11:39:56 +0100

    Vector verbeterd

--- Planets/Model/Vector.cs
+++ Planets/Model/Vector.cs
@@ -11,5 +11,20 @@
             X = x;
             Y = y;
         }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static Vector operator *(Vector v1, double scalar)
+        {
+            return new Vector(v1.X * scalar, v1.Y * scalar);
+        }
     }
 }
