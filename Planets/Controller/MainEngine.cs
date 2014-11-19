b399a094aa5f0467714498877d51c4ebcdb33abb
StanSwanborn <StanSwanborn@gmail.com>
2014-11-19 11:01:27 +0100

    MainEngine aangemaakt met folders

---
+++ Planets/Controller/MainEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planets.Controller
+{
+    class MainEngine
+    {
+
+        private PlanetsForm host;
+
+        public MainEngine(PlanetsForm host)
+        {
+            this.host = host;
+        }
+
+    }
+}
