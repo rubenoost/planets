6710d74afa5d1062cc03a27b3288976f59833777
StanSwanborn <StanSwanborn@gmail.com>
2014-11-19 11:22:49 +0100

    LOL foutje!

--- Planets/Utils.cs
+++ Planets/Utils.cs
@@ -6,26 +6,23 @@ using System.Threading.Tasks;
 
 namespace Planets
 {
-    class Utils
+    enum Direction
     {
-        enum Direction
+        up,
+        down,
+        left,
+        right
+    }
+
+    static class Utils
+    {
+
+        public static int StartMass = 300;
+
+        public static float CalcRadius(double mass)
         {
-            up,
-            down,
-            left,
-            right
-        }
-
-        static class Utils
-        {
-
-            public static int StartMass = 300;
-
-            public static float CalcRadius(double mass)
-            {
-                double R = Math.Sqrt(mass); // R = Wortel van M
-                return (float)(R * 2);
-            }
+            double R = Math.Sqrt(mass); // R = Wortel van M
+            return (float)(R * 2);
         }
     }
 }
