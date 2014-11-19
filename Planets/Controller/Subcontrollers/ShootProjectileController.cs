8bae9aca70531751fddb2970ca78a0d83695068f
Ruben Oost <mail@rubenoost.nl>
2014-11-19 13:33:38 +0100

    ShootprojectileController verbeterd (nog niet af)

--- Planets/Controller/Subcontrollers/ShootProjectileController.cs
+++ Planets/Controller/Subcontrollers/ShootProjectileController.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using Planets.Model;
+using Planets.View;
 
 namespace Planets.Controller.Subcontrollers
 {
@@ -8,9 +10,35 @@ namespace Planets.Controller.Subcontrollers
     /// </summary>
     public class ShootProjectileController
     {
-        public ShootProjectileController(Playfield pf, Control listenControl)
-        {
+        /// <summary>
+        /// The playfield used by this controller to shoot projectiles.
+        /// </summary>
+        public Playfield InternalPlayfield { get; private set; }
 
+        /// <summary>
+        /// The control used by this controller to listen on for mouse clicks.
+        /// </summary>
+        public Control InternalControl { get; private set; }
+
+        /// <summary>
+        /// Create new ShootProjectileController.
+        /// </summary>
+        /// <param name="pf">The playfield to shoot projectiles in.</param>
+        /// <param name="listenControl">The control to listen on for clicks.</param>
+        public ShootProjectileController(Playfield pf, GameView listenControl)
+        {
+            // Register event handlers
+            listenControl.Click += ClickCallback;
+        }
+
+        /// <summary>
+        /// Method called when click happens in listenControl.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClickCallback(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
