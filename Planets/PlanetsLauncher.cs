6f0f9d93d39efd18571d6ec6686dd3d4b0ff0940
Ruben Oost <mail@rubenoost.nl>
2014-11-19 09:36:06 +0100

    Development branch aangemaakt

--- Planets/PlanetsLauncher.cs
+++ Planets/PlanetsLauncher.cs
@@ -17,6 +17,7 @@ namespace Planets
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PlanetsForm());
+
         }
     }
 }
