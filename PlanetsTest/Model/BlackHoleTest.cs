d02fa63399c74c4ecfe7e40704c80c03d57364b5
JoelJansenD <joeljansend@gmail.com>
2014-11-25 09:35:06 +0100

    Tests done

--- PlanetsTest/Model/BlackHoleTest.cs
+++ PlanetsTest/Model/BlackHoleTest.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Planets.Model;
 
 namespace PlanetsTest.Model {
     [TestClass]
     public class BlackHoleTest {
         [TestMethod]
-        public void TestMethod1() {
+        public void BlackHoleIsBlackHole() {
+            BlackHole b = new BlackHole(new Vector(0, 0), new Vector(0, 0), 10.0, 5.0);
+            if(b is BlackHole) {
+                Assert.IsTrue(true, "Black Hole is Black Hole");
+            } else {
+                Assert.IsTrue(false, "Black Hole is no Black Hole");
+            }
+        }
+
+        [TestMethod]
+        public void BlackHoleValuesTrue() {
+            BlackHole b = new BlackHole(new Vector(0, 0), new Vector(0, 0), 10.0, 5.0);
+            double x = b.Gravity();
+
+            Assert.IsTrue(x == 10.0 + 5.0, "All values return correctly");
+            
         }
     }
 }
