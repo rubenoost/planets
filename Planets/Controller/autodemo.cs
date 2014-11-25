718c59c488638bd472e89ac2f5acab1d0cb4e837
Ruben Oost <mail@rubenoost.nl>
2014-11-25 11:52:02 +0100

    Commentaar toegevoegd

--- Planets/Controller/autodemo.cs
+++ Planets/Controller/autodemo.cs
@@ -1,52 +1,97 @@
 ﻿using System;
 using System.Drawing;
 using System.Threading;
+using Planets.Controller.Subcontrollers;
+using Planets.Model;
 
 namespace Planets.Controller
 {
     class Autodemo
     {
-        internal Model.Playfield field;
-        private Controller.Subcontrollers.ShootProjectileController spc;
+        /// <summary>
+        /// The field used by this autodemo
+        /// </summary>
+        internal Playfield Field;
 
-        public Autodemo(Model.Playfield p, Controller.Subcontrollers.ShootProjectileController s)
+        /// <summary>
+        /// The shootprojectilecontroller used by this autodemo
+        /// </summary>
+        internal ShootProjectileController Spc;
+
+        /// <summary>
+        /// Create new autodemo on given playfield that uses given ShootProjectileController
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="s"></param>
+        public Autodemo(Playfield p, ShootProjectileController s)
         {
-            field = p;
-            spc = s;
+            Field = p;
+            Spc = s;
         }
 
+        /// <summary>
+        /// Whether the key is pressed
+        /// </summary>
         public bool Kpressed
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Whether this autodemo is running
+        /// </summary>
         public bool running
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Run the autodemo
+        /// </summary>
         public void Run()
         {
-            var p = new Point(); var r = new Random();
+            // Create vars
+            var p = new Point();
+            var r = new Random();
+
+            // While key is pressed
             while (Kpressed)
             {
-                this.running = true;
-                p = new Point(r.Next(0, field.Size.Width), r.Next(0, field.Size.Height));
+                // Set running to true
+                running = true;
+
+                // Determine next click
+                p = new Point(r.Next(0, Field.Size.Width), r.Next(0, Field.Size.Height));
+
+                // Click 3 times and wait
                 for (int i = 0; i < 3; i++)
                 {
-                    spc.Clicked(p); field.LastAutoClickLocation = p; field.LastAutoClickMoment = DateTime.Now; Thread.Sleep(400);
+                    // Click
+                    Spc.Clicked(p);
+                    Field.LastAutoClickLocation = p;
+                    Field.LastAutoClickMoment = DateTime.Now;
+                    Thread.Sleep(400);
                 }
                 Thread.Sleep(1500);
             }
-            this.running = false;
+
+            // Set running to false
+            running = false;
         }
 
+        /// <summary>
+        /// Stop autodemo
+        /// </summary>
         public void Stop()
         {
             Kpressed = false;
         }
+
+        /// <summary>
+        /// Start autodemo
+        /// </summary>
         public void Start()
         {
             Kpressed = true;
