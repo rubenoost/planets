ff72ed393a0c1e0150170477ce02dafac7407d47
Martijn <martijnie96@gmail.com>
2014-11-27 14:47:07 +0100

    Komeet met staart

--- Planets/Properties/Resources.Designer.cs
+++ Planets/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace Planets.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap KomeetStaartje {
+            get {
+                object obj = ResourceManager.GetObject("KomeetStaartje", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap LogoFinal {
             get {
                 object obj = ResourceManager.GetObject("LogoFinal", resourceCulture);
