b46c2fc585b15ed59e23c4b6e31fa0f554c514fa
Vaarkamp <rickvaarkamp@gmail.com>
2014-11-28 15:00:01 +0100

    Omnomnomnom werkt lekker

--- Planets/Controller/PhysicsRules/CollidewithSmaller.cs
+++ Planets/Controller/PhysicsRules/CollidewithSmaller.cs
@@ -4,49 +4,31 @@ namespace Planets.Controller.PhysicsRules
 {
     class CollidewithSmaller : AbstractCollisionRule
     {
+
+        public void change(GameObject go1, GameObject go2, Playfield pf)
+        {
+            if (!(go2 is Player))
+            {
+                go1.mass += go2.mass;
+                go2.mass = 1;
+                go1.Location += ((go2.Location - go1.Location)*(go2.mass/(go1.mass+ go2.mass)));
+                go1.DV = ((go1.mass*go1.DV) + (go2.mass*go2.DV))/(go1.mass + go2.mass);
+                pf.BOT.Remove(go2);
+            }
+        }
+
+
         protected override void DoCollision(Playfield pf, GameObject go1, GameObject go2, double ms)
         {
             if (go1.IntersectsWith(go2))
             {
-                if (go1.mass > go2.mass)
+                if (go1.mass >= go2.mass)
                 {
-                    if (!(go2 is Player))
-                    {
-                        go1.mass += go2.mass;
-                        go2.mass = 1;
-                        go1.Location = 0.5 * (go1.Location + go2.Location);
-                        pf.BOT.Remove(go2);
-                    }
+                    change(go1,go2,pf);
                 }
                 else if (go2.mass > go1.mass)
                 {
-                    if (!(go1 is Player))
-                    {
-                        go2.mass += go1.mass;
-                        go1.mass = 1;
-                        go2.Location = 0.5 *(go1.Location + go2.Location);
-                        pf.BOT.Remove(go1);
-                    }
-                }
-                else if (go1.mass == go2.mass)
-                {
-                    if (!(go1 is Player))
-                    {
-                        go2.mass += go1.mass;
-                        go1.mass = 1;
-                        go1.Location = 0.5 * (go1.Location + go2.Location);
-                        pf.BOT.Remove(go1);
-                    }
-                }
-                else if (go2.mass == go1.mass)
-                {
-                    if (!(go2 is Player))
-                    {
-                        go1.mass += go2.mass;
-                        go2.mass = 1;
-                        go2.Location = 0.5 * (go1.Location + go2.Location);
-                        pf.BOT.Remove(go2);
-                    }
+                    change(go2,go1,pf);
                 }
             }
         }
