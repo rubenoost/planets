56b9a3de711f93d67e7e92af0c4fb57b63e9727e
Ruben Oost <mail@rubenoost.nl>
2014-11-28 11:59:46 +0100

    Blackhole bugfix

--- Planets/Controller/PhysicsRules/BlackHoleRule.cs
+++ Planets/Controller/PhysicsRules/BlackHoleRule.cs
@@ -6,7 +6,7 @@ namespace Planets.Controller.PhysicsRules
     class BlackHoleRule : AbstractGameRule
     {
         // changes the speed
-        private double JoelConstante = 1.0;
+        private double JoelConstante = 10.0;
 
         protected override void ExecuteRule(Playfield pf, double ms)
         {
@@ -22,7 +22,7 @@ namespace Planets.Controller.PhysicsRules
                         if (g != g2 && !(g2 is Player))
                         {
                             Vector V = g.Location - g2.Location;
-                            double Fg = JoelConstante * ((g2.mass * g.mass) / (V.Length() * V.Length()));
+                            double Fg = JoelConstante * (g.mass / (V.Length() * V.Length()));
                             // Speed of projectile gets updated
                             g2.DV += V.ScaleToLength(Fg * (ms / 1000.0));
                         }
