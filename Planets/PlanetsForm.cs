16af692b40353e1907b179190d4a63412e12fc84
Vaarkamp <rickvaarkamp@gmail.com>
2014-12-03 16:05:14 +0100

    Probeersel weggehaald

--- Planets/PlanetsForm.cs
+++ Planets/PlanetsForm.cs
@@ -12,23 +12,12 @@ namespace Planets
 
         public PlanetsForm()
         {
-            Timer t = new Timer { Enabled = true };
-            t.Interval = 5000;
-            t.Tick += t_Tick;
-
-            t.Start();
-
             InitializeComponent();
             DoubleBuffered = true;
             engine = new MainEngine(this);
 
             // Event handlers
             Closed += (sender, args) => Process.GetCurrentProcess().Kill();
-        }
-
-        void t_Tick(object sender, EventArgs e)
-        {
-            this.Close();
         }
     }
 }
