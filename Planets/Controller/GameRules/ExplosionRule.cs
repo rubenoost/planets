6ba7b2c50308d69fd068d1e11d1f51bec736fc9a
Bart Willemsen <b.willemsen8@gmail.com>
2014-12-09 10:44:08 +0100

    Explosie fix

--- Planets/Controller/GameRules/ExplosionRule.cs
+++ Planets/Controller/GameRules/ExplosionRule.cs
@@ -35,6 +35,7 @@ namespace Planets.Controller.PhysicsRules
 
 			// Go boom!
 			double lostMass = goPlayer.Mass / 2;
+			goPlayer.Mass -= lostMass;
 
 			// TODO: We should probably create an epic explosion before removing the object.
 			pf.BOT.Remove(goExplodes);
@@ -43,15 +44,21 @@ namespace Planets.Controller.PhysicsRules
 
 			double massPool = lostMass;
 			while (massPool > 0) {
-				double mass = random.Next(1000, 3000);
+				double mass = 0.0;
+
+				// We don't want to generate more mass than is actually lost by the player.
+				if (mass <= 1000) {
+					mass = random.Next(500, (int)massPool);
+				} else {
+					mass = random.Next(500, 1000);
+				}
+
 				Vector createLocation = goExplodes.Location + new Vector(random.Next(-50, 100), random.Next(-50, 100));
 				GameObject debris = new GameObject(createLocation, new Vector(random.Next(-500, 500), random.Next(-500, 500)), mass);
 
 				pf.BOT.Add(debris);
 				massPool -= mass;
 			}
-
-			goPlayer.Mass -= lostMass;
 		}
 	}
 }
