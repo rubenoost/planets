ff5e1b3c35f28406bb343f580dd1a49b1ffefb39
Martijn <martijnie96@gmail.com>
2014-12-09 10:27:06 +0100

    Animation object

---
+++ Planets/Model/GameObjects/Animation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planets.Model.GameObjects
+{
+    class Animation : GameObject
+    {
+                public Animation(Vector location, Vector velocity, double mass)
+            : base(location, velocity, mass, Rule.NONE)
+
+        {
+
+        }
+    }
+}
