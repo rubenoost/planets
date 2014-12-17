d7aa6e166b7e96b7c897d021eeab6287d7204b18
Ruben Oost <mail@rubenoost.nl>
2014-12-17 08:28:39 +0100

    Antimatter fix

--- Planets/Controller/GameRules/DynamicEatRule.cs
+++ Planets/Controller/GameRules/DynamicEatRule.cs
@@ -39,7 +39,8 @@ namespace Planets.Controller.GameRules
                 // Check for mass of large gameobject
                 if (LostMass >= gL.Mass)
                 {
-                    pf.BOT.Remove(gL);
+                    if(gL != pf.CurrentPlayer)
+                        pf.BOT.Remove(gL);
                 }
                 else
                     gL.Mass -= LostMass;
