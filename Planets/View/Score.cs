8a6ecd97def0999fbe0fc1de39f2835cf1d41741
Martijn <martijnie96@gmail.com>
2014-12-18 11:01:24 +0100

    Fix asd asd

---
+++ Planets/View/Score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planets.View
+{
+    public class Score : GameView
+    {
+        public void ModifyScore(int Score)
+            {
+                int CurrentScore = Convert.ToInt32(ScoreLabel.Text);
+                ScoreLabel.Text = Convert.ToString(CurrentScore + Score);
+            }
+
+    }
+}
