9a7502a315c9aefc4840888c09eabb10e3402b88
Martijn <martijnie96@gmail.com>
2014-12-18 11:44:48 +0100

    Plaatje

--- Planets/Properties/Resources.Designer.cs
+++ Planets/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace Planets.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap HighScoreLogo {
+            get {
+                object obj = ResourceManager.GetObject("HighScoreLogo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Hole1 {
             get {
                 object obj = ResourceManager.GetObject("Hole1", resourceCulture);
