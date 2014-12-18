908eded160222619853982d4bdc43ff4ddfced7d
Ruben Oost <mail@rubenoost.nl>
2014-12-18 09:44:25 +0100

    Commentaar bijgevoegd

--- Planets/Controller/GameRules/GameTargets/GameTargetGetLargest.cs
+++ Planets/Controller/GameRules/GameTargets/GameTargetGetLargest.cs
@@ -2,8 +2,16 @@
 
 namespace Planets.Controller.GameRules.GameTargets
 {
+    /// <summary>
+    /// GameTarget to get the largest.
+    /// </summary>
     public class GameTargetGetLargest : IGameTarget
     {
+        /// <summary>
+        /// Checks whether player is largest now.
+        /// </summary>
+        /// <param name="ge">The GameEngine.</param>
+        /// <returns>Boolean indicating whether the target is reached.</returns>
         public bool IsTargetReached(GameEngine ge)
         {
             // Get player mass, buffered for performance
