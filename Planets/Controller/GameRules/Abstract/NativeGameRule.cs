22d86579ab819c3092eebb0e910959149d5b6bed
Ruben Oost <mail@rubenoost.nl>
2014-12-18 09:42:40 +0100

    Commentaar bijgevoegd

--- Planets/Controller/GameRules/Abstract/NativeGameRule.cs
+++ Planets/Controller/GameRules/Abstract/NativeGameRule.cs
@@ -1,7 +1,15 @@
 ﻿namespace Planets.Controller.GameRules.Abstract
 {
+    /// <summary>
+    /// This class is used as the native gamerule, a GameEngine is given to this function to get access to every function.
+    /// </summary>
     public interface INativeGameRule
     {
+        /// <summary>
+        /// Execute this GameRule.
+        /// </summary>
+        /// <param name="ge">The GameEngine this GameRule is running in</param>
+        /// <param name="ms">The amount of milliseconds elapsed since the last call.</param>
         void Execute(GameEngine ge, double ms);
     }
 }
