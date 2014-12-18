05ee7f920230f9612bceadb60bd197f08718f7d4
Martijn <martijnie96@gmail.com>
2014-12-18 11:11:26 +0100

    Cleanup

--- Planets/Model/ScoreBoard.cs
+++ Planets/Model/ScoreBoard.cs
@@ -4,47 +4,49 @@ using System.Xml;
 
 namespace Planets.Model
 {
-	public class ScoreBoard
-	{
-		public int Total { get; private set; }
+    public class ScoreBoard
+    {
+        public int Total { get; private set; }
 
-		public List<Score> Scores { get; private set; }
+        public List<Score> Scores { get; private set; }
 
-		public ScoreBoard()
-		{
-			Scores = new List<Score>();
-		}
+        public ScoreBoard()
+        {
+            Scores = new List<Score>();
+        }
 
-		public void AddScore(Score score)
-		{
-			Scores.Add(score);
-			Total += score.Value;
-		}
+        public void AddScore(Score score)
+        {
+            Scores.Add(score);
+            Total += score.Value;
+        }
 
-		public void CheckStamps()
-		{
-			for (int i = Scores.Count - 1; i >= 0; i--) {
-				TimeSpan span = DateTime.Now - Scores[i].Stamp;
-				if (span.TotalMilliseconds >= 2000)
-					Scores.Remove(Scores[i]);
-			}
-		}
+        public void CheckStamps()
+        {
+            for (int i = Scores.Count - 1; i >= 0; i--)
+            {
+                TimeSpan span = DateTime.Now - Scores[i].Stamp;
+                if (span.TotalMilliseconds >= 2000)
+                    Scores.Remove(Scores[i]);
+            }
+        }
 
-		public static int[] getHighScore()
-		{
+        public static int[] getHighScore()
+        {
             String filepath = "scores.xml";
 
-			XmlDocument xd = new XmlDocument();
-			xd.Load(filepath);
+            XmlDocument xd = new XmlDocument();
+            xd.Load(filepath);
 
-			XmlNodeList nodelist = xd.SelectNodes("/scores");
+            XmlNodeList nodelist = xd.SelectNodes("/scores");
 
-			List<int> scores = new List<int>();
-			for (int i = 0; i < nodelist.Count; i++) {
-				scores.Add(Convert.ToInt16(nodelist[i].SelectSingleNode("score").ToString()));
-			}
+            List<int> scores = new List<int>();
+            for (int i = 0; i < nodelist.Count; i++)
+            {
+                scores.Add(Convert.ToInt16(nodelist[i].SelectSingleNode("score").ToString()));
+            }
 
-			return scores.ToArray();
-		}
-	}
+            return scores.ToArray();
+        }
+    }
 }
