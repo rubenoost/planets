edfc33ac20b525650da52dc0fae449be1abe85a4
Martijn <martijnie96@gmail.com>
2014-12-18 11:48:52 +0100

    Revert "Unused"
    This reverts commit 2d84db559f814202787c9800f12e918277e01246.

--- Planets/PlanetsForm.cs
+++ Planets/PlanetsForm.cs
@@ -7,10 +7,13 @@ namespace Planets
     public partial class PlanetsForm : Form
     {
 
+        MainEngine engine;
+
         public PlanetsForm()
         {
             InitializeComponent();
             DoubleBuffered = true;
+            engine = new MainEngine(this);
 
             // Event handlers
             Closed += (sender, args) => Process.GetCurrentProcess().Kill();
