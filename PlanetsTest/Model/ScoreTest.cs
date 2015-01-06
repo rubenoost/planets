4c885eb84b20cea9e6058191f583224bcb1ab769
Bart Willemsen <b.willemsen8@gmail.com>
2015-01-06 13:56:45 +0100

    Tests voor score toegevoegd

---
+++ PlanetsTest/Model/ScoreTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Planets.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetsTest.Model
+{
+	[TestClass]
+	class ScoreTest
+	{
+		[TestMethod]
+		public void testWriteScoreShouldWriteScoreIfNoFileExists()
+		{
+			ScoreBoard.WriteScore(10);
+
+			Assert.IsTrue(File.Exists(ScoreBoard.Filepath));
+		}
+
+		[TestMethod]
+		public void testReadMethodReadsScoreFromFileSystem()
+		{
+			ScoreBoard.WriteScore(10);
+			Assert.AreEqual(10, ScoreBoard.GetHighScore());
+		}
+	}
+}
