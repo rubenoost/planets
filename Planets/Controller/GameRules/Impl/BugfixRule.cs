50e9e31dd4a79fcf2e1ffefe1c7dab1920dcc363
Ruben Oost <mail@rubenoost.nl>
2015-01-07 11:22:42 +0100

    Commentaar bij BugFixRule

--- Planets/Controller/GameRules/Impl/BugfixRule.cs
+++ Planets/Controller/GameRules/Impl/BugfixRule.cs
@@ -5,15 +5,26 @@ namespace Planets.Controller.GameRules.Impl
 {
     public class BugfixRule : AbstractGameRule
     {
+        /// <summary>
+        /// Maximum speed of a GameObject.
+        /// </summary>
         private const double MaxSpeed = 1920.0d;
+
+        /// <summary>
+        /// The minimal mass of a GameObject.
+        /// </summary>
         private const double MinMass = 30.0d;
 
         protected override void ExecuteRule(Playfield pf, double ms)
         {
+            // Iterate through all gameobjects
             pf.GameObjects.Iterate(g =>
             {
+                // If object is too small, delete from field
                 if(g.Mass < MinMass)
                     pf.GameObjects.Remove(g);
+
+                // If speed is to high, scale down to maximum speed
                 if (g.Dv.Length() > MaxSpeed)
                     g.Dv = g.Dv.ScaleToLength(MaxSpeed);
             });
